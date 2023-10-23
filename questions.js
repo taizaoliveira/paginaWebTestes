7bb364802310600a4ed964bbfad26df078185020
taizaoliveira <taizapauladeoliveiralima@gmail.com>
2023-10-23 17:38:49 -0300

    Melhorias

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quem é o protagonista de Jujutsu Kaisen?",
+    answer: "Yuji Itadori",
+    options: [
+      "Gojo Satoru",
+      "Megumi Fushiguro",
+      "Yuji Itadori",
+      "Toge Inumaki"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual é o nome da maldição amaldiçoada que é selada no dedo de Sukuna em Jujutsu Kaisen?",
+    answer: "Ryomen Sukuna",
+    options: [
+      "Mahito",
+      "Cursed Spirit",
+      "Sakuna",
+      "Ryomen Sukuna"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual é o objetivo da Escola Jujutsu na série Jujutsu Kaisen?",
+    answer: "Eliminar maldições",
+    options: [
+      "Eliminar maldições",
+      "Controlar maldições",
+      "Usar maldições para o bem",
+      "Criar maldições poderosas"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual é a habilidade especial de Megumi Fushiguro em Jujutsu Kaisen?",
+    answer: "Conjurar espíritos protetores",
+    options: [
+      "Manipulação de fogo",
+      "Controle de maldições",
+      "Cura instantânea",
+      "Conjurar espíritos protetores"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual é o título oficial de Satoru Gojo na Escola Jujutsu?",
+    answer: "Professor Exemplar",
+    options: [
+      "Professor Exemplar",
+      "Mestre das Maldições",
+      "Rei das Maldições",
+      "Líder dos Jujutsu Sorcerers"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
